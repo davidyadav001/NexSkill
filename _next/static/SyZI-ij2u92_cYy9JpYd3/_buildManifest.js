0a4884d99f8c74acfd389e9f5b9b551e4af00a7f
davidyadav001 <davidyadav9818@gmail.com>
2026-03-11 21:05:38 +0545

    Updates

--- _next/static/SyZI-ij2u92_cYy9JpYd3/_buildManifest.js
+++ _next/static/SyZI-ij2u92_cYy9JpYd3/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/NexSkill//_next/:path+",
+        "destination": "/NexSkill/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
